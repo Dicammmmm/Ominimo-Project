ae6ebeb792a37ff5a6e29f7d2287eebb45c57549
Dicammmmm <marko.madic13@outlook.com>
2025-10-26 16:34:34 +0100

    Added incremental loading to the models

--- dbt_project/ominimo_transform/models/staging/stg_policies.sql
+++ dbt_project/ominimo_transform/models/staging/stg_policies.sql
@@ -1,3 +1,10 @@
+{{ 
+    config(
+        materialized='incremental',
+        unique_key='policy_id'
+    )
+}}
+
 WITH source AS (
     SELECT * FROM {{ source('raw', 'policies_raw') }}
 )
@@ -6,12 +13,16 @@ SELECT
     policy_id::VARCHAR(16) AS policy_id,
     quote_id::VARCHAR(16) AS quote_id,
     customer_id::VARCHAR(16) AS customer_id,
-    created_at::TIMESTAMP AS created_at,
     vehicle_make::VARCHAR(16) AS vehicle_make,
     vehicle_model::VARCHAR(16) AS vehicle_model,
     vehicle_year::INTEGER AS vehicle_year,
     status::VARCHAR(16) AS status,
     source_file::VARCHAR(64) AS source_file,
+    created_at::TIMESTAMP AS created_at,
     CURRENT_TIMESTAMP AS loaded_at
 FROM
     source
+
+{% if is_incremental() %}
+    WHERE created_at::TIMESTAMP > (SELECT MAX(created_at) FROM {{ this }})
+{% endif %}
--- dbt_project/ominimo_transform/models/staging/stg_quotes.sql
+++ dbt_project/ominimo_transform/models/staging/stg_quotes.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='quote_id'
+    )
+}}
 WITH source AS (
     SELECT * FROM {{ source('raw', 'quotes_raw') }}
 )
@@ -7,8 +13,12 @@ SELECT
     product::VARCHAR(16) AS product,
     price::DOUBLE PRECISION AS price,
     technical_price::DOUBLE PRECISION AS technical_price,
-    created_at::TIMESTAMP AS created_at,
     'quotes_raw.parquet'::VARCHAR(64) AS source_file,
+    created_at::TIMESTAMP AS created_at,
     CURRENT_TIMESTAMP AS loaded_at
 FROM
     source
+
+{% if is_incremental() %}
+    WHERE created_at::TIMESTAMP > (SELECT MAX(created_at) FROM {{ this }})
+{% endif %}
